b1063cefe8802b37deaaf93649fdb526acaefe8e
Alfredo <alfredofontinele08@gmail.com>
2023-02-16 13:36:34 -0300

    test : route not found

---
+++ README.md
@@ -0,0 +1,19 @@
+# API Utilizada para pesquisa => 'https://github.com/cyberboysumanjay/Inshorts-News-API'
+
+# Endpoints
+
+## This API supports category wise news. Here is a complete list of all categories.
+
+    all
+    national //Indian News only
+    business
+    sports
+    world
+    politics
+    technology
+    startup
+    entertainment
+    miscellaneous
+    hatke
+    science
+    automobile
--- src/pages/not-found-page/index.tsx
+++ src/pages/not-found-page/index.tsx
@@ -1,8 +1,23 @@
+import { Flex, Button, Text } from "@chakra-ui/react";
+import { useAPI } from "../../context/useApi";
+import { Colors } from "../../styles/colors";
+
 export const NotFound = () => {
-    document.title = 'Not Found'
+    const { navigate } = useAPI();
+    document.title = "Not Found";
     return (
-        <>
-            <h1>Not Found</h1>
-        </>
-    )
-}
+        <Flex
+            flexDir={"column"}
+            justifyContent={"center"}
+            alignItems={"center"}
+            minH="100vh"
+            gap={10}
+            bg={Colors.dark}
+        >
+            <Text fontSize={"4xl"} color={Colors.default}>
+                Page Not Found
+            </Text>
+            <Button onClick={() => navigate("/")}>Voltar à Home</Button>
+        </Flex>
+    );
+};
